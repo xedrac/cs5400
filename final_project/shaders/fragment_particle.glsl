c26fa9040fdc0b7854cd18ce5db98f8a01de4992
bronyaur <pete.ownby@gmail.com>
2013-04-16 02:48:38 -0600

    Forgot to add the shaders

---
+++ final_project/shaders/fragment_particle.glsl
@@ -0,0 +1,7 @@
+varying vec4 result_color;
+
+void main(void) 
+{
+  gl_FragColor = result_color;
+  //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
---
+++ final_project/shaders/vertex_particle.glsl
@@ -0,0 +1,15 @@
+attribute vec4 vPosition;
+attribute vec4 vColor;
+varying vec4 result_color;
+
+uniform mat4 M;
+uniform mat4 V;
+uniform mat4 P;
+
+void main() 
+{
+	gl_Position = P * V * M * vPosition;
+	//gl_Position = vPosition;
+    result_color = vColor;
+}
+
